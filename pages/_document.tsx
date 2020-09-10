8ce53d3e63cac9dc8ef671134711286744b3d8f7
Ethan Mick <ethan@ethanmick.com>
2020-09-09 21:44:20 -0400

    Updated App Icon/Favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -28,7 +28,6 @@ class MyDocument extends Document {
             sizes="16x16"
             href="/favicon-16x16.png"
           />
-          <link rel="apple-touch-startup-image" href="/launch.png" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <link rel="shortcut icon" href="/favicon.ico" />
           <meta name="apple-mobile-web-app-status-bar-style" content="black" />
