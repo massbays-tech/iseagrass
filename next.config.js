e4bbb369a319840be1902ff2a0ae2187be588f9d
Ethan Mick <ethan@ethanmick.com>
2021-07-12 19:56:09 -0400

    Update packages

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = withOffline({
   workboxOpts: {
     swDest: process.env.NEXT_EXPORT
       ? 'service-worker.js'
-      : 'static/service-worker.js',
+      : 'public/service-worker.js',
     runtimeCaching: [
       {
         urlPattern: /^https?.*/,
